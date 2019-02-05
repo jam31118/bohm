315673b0a3b10ff1d9a904a8dcb5c16d573aa3d1
jam31118 <jam45@naver.com>
2019-02-05 23:41:26 +0900

    added several tableau including 5th order Dormand-Prince method

--- bohm/rk/tableau.py
+++ bohm/rk/tableau.py
@@ -42,10 +42,10 @@ class Butcher_Tableau(object):
         
     @staticmethod
     def has_consistency(A, b, c, b_star):
-        _good_A_c = np.all(c == np.sum(A, axis=1))
-        _good_b = b.sum() == 1.0
+        _good_A_c = np.all(np.isclose(c, np.sum(A, axis=1)))
+        _good_b = np.isclose(b.sum(), 1.0)
         _good_b_star = True
-        if b_star is not None: _good_b_star = b_star.sum() == 1.0
+        if b_star is not None: _good_b_star = np.isclose(b_star.sum(), 1.0)
         _all_good = _good_A_c and _good_b and _good_b_star
         return _all_good
 
@@ -81,4 +81,42 @@ b_star = np.array([7.0/24.0, 1.0/4.0, 1.0/3.0, 1.0/8.0], dtype=float)
 c = np.array([0.0, 0.5, 0.75, 1.0], dtype=float)
 b_table_Bogacki_Shampine = Butcher_Tableau(A, b, c, b_star=b_star)
 
+## Fehlberg
+N_s = 6
+A = np.zeros((N_s,N_s), dtype=float)
+A[1,:1] = 0.25
+A[2,:2] = [3/32, 9/32]
+A[3,:3] = [1932/2197,-7200/2197,7296/2197]
+A[4,:4] = [439/216, -8, 3680/513, -845/4104]
+A[5,:5] = [-8/27, 2, -3544/2565, 1859/4104, -11/40]
+b = np.array([16/135, 0, 6656/12825, 28561/56430, -9/50, 2/55], dtype=float)
+b_star = np.array([25/216, 0, 1408/2565, 2197/4104, -1/5, 0], dtype=float)
+c = np.array([0.0, 0.25, 0.375, 12/13, 1.0, 0.5], dtype=float)
+b_table_Fehlberg = Butcher_Tableau(A, b, c, b_star=b_star)
 
+## Cash-Karp
+N_s = 6
+A = np.zeros((N_s,N_s), dtype=float)
+A[1,:1] = 0.2
+A[2,:2] = [3/40, 9/40]
+A[3,:3] = [3/10, -9/10, 6/5]
+A[4,:4] = [-11/54, 5/2, -70/27, 35/27]
+A[5,:5] = [1631/55296, 175/512, 575/13824, 44275/110592, 253/4096]
+b = np.array([37/378, 0, 250/621, 125/594, 0, 512/1771], dtype=float)
+b_star = np.array([2825/27648, 0, 18575/48384, 13525/55296, 277/14336, 1/4], dtype=float)
+c = np.array([0.0, 0.2, 0.3, 0.6, 1.0, 0.875], dtype=float)
+b_table_Cash_Karp = Butcher_Tableau(A, b, c, b_star=b_star)
+
+## Dormand-Prince
+N_s = 7
+A = np.zeros((N_s,N_s), dtype=float)
+A[1,:1] = 0.2
+A[2,:2] = [3/40, 9/40]
+A[3,:3] = [44/45, -56/15, 32/9]
+A[4,:4] = [19372/6561, -25360/2187, 64448/6561, -212/729]
+A[5,:5] = [9017/3168, -355/33, 46732/5247, 49/176, -5103/18656]
+A[6,:6] = [35/384, 0, 500/1113, 125/192, -2187/6784, 11/84]
+b = np.array([35/384, 0, 500/1113, 125/192, -2187/6784, 11/84, 0], dtype=float)
+b_star = np.array([5179/57600, 0, 7571/16695, 393/640, -92097/339200, 187/2100, 1/40], dtype=float)
+c = np.array([0.0, 0.2, 0.3, 0.8, 8/9, 1.0, 1.0], dtype=float)
+b_table_Dormand_Prince = Butcher_Tableau(A, b, c, b_star=b_star)
